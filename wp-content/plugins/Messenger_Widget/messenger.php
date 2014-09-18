9c5cfebbcb3fc7f7261558cbb07598129e0b3ac2
stevenay <naylinaung.softwarearchitect@gmail.com>
2014-09-19 03:53:23 +0630

    Widget Test Finished

--- wp-content/plugins/Messenger_Widget/messenger.php
+++ wp-content/plugins/Messenger_Widget/messenger.php
@@ -1,5 +1,6 @@
 <?php
 error_reporting(E_ALL);
+
 /*
 Plugin Name: Messager Widget
 Plugin URI: http://naylinaung.me
@@ -11,27 +12,56 @@ Version: 1.0
 
 class Messager extends WP_Widget {
 
-    function __construct() 
+    function __construct()
     {
         $params = array(
             'description' => 'Display a message to readers',
-            'name' => 'Messager'
+            'name'        => 'Messager'
         );
 
         parent::__construct('Messager', '', $params);
     }
 
-    function form()
+    function form($instance)
     {
-      
+        extract($instance);
+        ?>
+        <p>
+            <label for="<?php echo $this->get_field_id('title') ?>">Title: </label>
+            <input
+                type="text"
+                class="widefat"
+                id="<?php echo $this->get_field_id('title'); ?>"
+                name="<?php echo $this->get_field_name('title'); ?>"
+                value="<?php if ( isset($title) ) echo esc_attr($title); ?>" />
+        </p>
+
+        <p>
+            <label for="<?php echo $this->get_field_id('description') ?>">Description: </label>
+            <textarea
+                class="widefat"
+                id="<?php echo $this->get_field_id('description'); ?>"
+                name="<?php echo $this->get_field_name('description'); ?>"><?php if ( isset($description) ) echo esc_attr($description); ?></textarea>
+        </p>
+    <?php
+
     }
 
-    function widget()
+    function widget($args, $instance)
     {
+        extract($args);
+        extract($instance);
 
+        $title = apply_filters('widget_title', $title);
+        $description = apply_filters('widget_description', $description);
+
+        echo $before_widget;
+        echo $before_title . $title . $after_title;
+        echo "<p>$description</p>";
+        echo $after_widget;
     }
 }
 
-add_action('widgets_init', function() {
+add_action('widgets_init', function () {
     register_widget('Messager');
 });
